3b4aef12d45be1ab5fe02892856364e19418a096
Ivan Covach <quinelleave@gmail.com>
2022-08-08 19:22:13 +0300

    Complete task 1.15

---
+++ code/chapter_1/tasks/task_1.15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+float FahrToCel(float);
+
+int main()
+{
+    for (int fahr = 0; fahr <= UPPER; fahr += STEP)
+        printf("%3d %6.1f\n", fahr, FahrToCel(fahr));
+    return 0;
+}
+
+float FahrToCel(float fahr) {
+    return fahr * 1.8 + 32;
+}
