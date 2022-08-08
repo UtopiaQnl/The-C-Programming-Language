fbe2a875d7471cd0f44e0f63f4ca6df86ff2de8f
Ivan Covach <quinelleave@gmail.com>
2022-08-08 13:59:36 +0300

    Add new file 1.2_table_temperature.c

---
+++ code/chapter_1/1.2_table_temperature.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* печатает таблицы температур по Френгейту
+ * и Цельсию для fahr = 0, 20, ..., 300 */
+
+main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;  // нижняя граница таблицы температур
+    upper = 300;  // верхняя граница
+    step = 20;  // шаг
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%3d %6d\n", fahr, celsius); // "%d\t%d\n"
+        fahr = fahr + step;
+    }
+}
