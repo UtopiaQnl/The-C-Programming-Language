cff738ee8d316d2773a97565f0fccdb7e6246df3
Ivan Covach <quinelleave@gmail.com>
2022-08-08 15:16:20 +0300

    Change 1.5.1_copy_file.c [task 1.7]
    Add output values of EOF const.

--- code/chapter_1/input_output_1.5/1.5.1_copy_file.c
+++ code/chapter_1/input_output_1.5/1.5.1_copy_file.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/* копирование ввода на вывод; 2-я версия */
+/* копирование ввода на вывод; 3-я версия */
 main()
 {
     int c;
-    while (c = getchar() != EOF) {
-        putchar(c);
+    while ((c = getchar()) != EOF) {
+        printf("%c [EOF] = %c (%d)\n", c, EOF, EOF);
     }
 }
