182c12d238600eb344a2096cbba460ebd8a2e5c2
Ivan Covach <quinelleave@gmail.com>
2022-08-08 15:54:39 +0300

    Change 1.5.2_count_chars.c [update 1]
    Replace while cycle on for cycle.

--- code/chapter_1/input_output_1.5/1.5.2_count_chars.c
+++ code/chapter_1/input_output_1.5/1.5.2_count_chars.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/* подсчет вводимых символов; 1-я версия */
+/* подсчет вводимых символов; 2-я версия */
 main()
 {
-    long nc;
-    nc = 0;
-    while (getchar() != EOF)
-        ++nc;
-    printf("count = %ld\n", nc);
+    double nc;
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("count = %.0f\n", nc);
 }
