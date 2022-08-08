037f6d9dd80d3f5ff60bb08caed76e4d08aafaaa
Ivan Covach <quinelleave@gmail.com>
2022-08-08 17:12:44 +0300

    Add new file 1.5.3_count_line.c

---
+++ code/chapter_1/input_output_1.5/1.5.3_count_line.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* подсчет строк входного потока */
+main()
+{
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("count line = %d\n", nl);
+}
