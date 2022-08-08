42e97aad1749087e4e41624bb5651f0a8f6b878a
Ivan Covach <quinelleave@gmail.com>
2022-08-08 17:48:18 +0300

    Add new file 1.5.4_count_words.c

---
+++ code/chapter_1/input_output_1.5/1.5.4_count_words.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define IN 1 /* внутри слова */
+#define OUT 0 /* вне слова */
+
+/* подсчет строк, слов и символов */
+main()
+{
+    int c, nl, nw, nc, state;
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        nc++;
+        if (c == '\n')
+            nl++;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (OUT == state) {
+            state = IN;
+            nw++;
+        }
+    }
+    printf("lines: %d\nwords: %d\ncharacters: %d\n", nl, nw, nc);
+}
