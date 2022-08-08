8b22f8b5c625f66df9c8974e8525370465482df5
Ivan Covach <quinelleave@gmail.com>
2022-08-08 18:15:26 +0300

    Add new file 1.6_array.c

---
+++ code/chapter_1/1.6_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* подсчет цифр, символов-разделителей и прочих символов */
+
+main()
+{
+    int c, i, nwhite, nother;
+    int ndigit[10];  // = {0} ???
+    nwhite = nother = 0;
+    for (i = 0; i < 10; i++)  // ???
+        ndigit[i] = 0;         // why?!
+    while ((c = getchar()) != EOF) {
+        if ('0' <= c && c <= '9')
+            ndigit[c - '0']++;
+        else if (c == ' ' || c == '\t' || c == '\n')
+            nwhite++;
+        else
+            nother++;
+    }
+    printf("цифры =");
+    for (i = 0; i < 10; i++)
+        printf(" %d", ndigit[i]);
+    printf(", символы-разделители = %d, прочие = %d\n", nwhite, nother);
+}
